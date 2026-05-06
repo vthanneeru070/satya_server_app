d31f70be1a520ac92206d9afb59f40295f3f1a49
Venkatesh070 <thanneeruvenkey007@email.com>
2026-05-06 13:47:07 +0530

    Added cros

--- src/app.js
+++ src/app.js
@@ -13,7 +13,12 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://satya-devotte-app-yc6c-venkats-projects-9161ee90.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(requestLogger);
 app.use(rateLimiter);
 app.use(express.json({ limit: "1mb" }));
