6dccb000bdc85f2c1190f819e1ba4baa88786ad1
Your Name <you@example.com>
2026-05-21 14:18:31 +0530

    Added cros

--- src/app.js
+++ src/app.js
@@ -20,11 +20,17 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://satya-devotte-app-yc6c.vercel.app", "https://satya-devotte-app-yc6c-venkats-projects-9161ee90.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://satya-devotte-app-yc6c.vercel.app",
+      "https://satya-devotte-app-yc6c-venkats-projects-9161ee90.vercel.app",
+      "http://18.209.102.86"
+    ],
     credentials: true,
   })
 );
-app.use(requestLogger);
+
+
 // app.use(rateLimiter);
 
 // IMPORTANT: payment webhooks MUST receive the raw body so HMAC signatures can
